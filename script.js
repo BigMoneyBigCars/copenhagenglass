9854cfe41089894dd2e026ff8fb7a481b7728b13
BigMoneyBigCars <l.stausgaard@gmail.com>
2019-12-16 13:11:49 +0100

    Footer lavet desktop/mobil

--- script.js
+++ script.js
@@ -28,3 +28,25 @@
 
 
        /* footer kode*/
+
+       document.addEventListener("DOMContentLoaded", getJson);
+       let aabningstider;
+
+       async function getJson() {
+
+           let jsonData = await fetch("https://mostvalue.dk/Copenhagenglass/wordpress/wp-json/wp/v2/aabningstid");
+
+           aabningstider = await jsonData.json();
+           console.log(aabningstider);
+           vis();
+       };
+
+       function vis() {
+           document.querySelector(".mandag").textContent = aabningstider[0].mandag;
+           document.querySelector(".tirsdag").textContent = aabningstider[0].tirsdag;
+           document.querySelector(".onsdag").textContent = aabningstider[0].onsdag;
+           document.querySelector(".torsdag").textContent = aabningstider[0].torsdag;
+           document.querySelector(".fredag").textContent = aabningstider[0].fredag;
+           document.querySelector(".loerdag").textContent = aabningstider[0].loerdag;
+           document.querySelector(".soendag").textContent = aabningstider[0].soendag;
+       }
